cc30e0bd636e2c15b9f3f7434f04f5d4c2792755
Alban Dericbourg <alban@dericbourg.net>
2013-05-28 15:46:52 +0200

    Add angular-strap to use bootstrap datepicker.
     * Plus some cleasing
     * Plus upgrade to latest AngularJS version

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('gestionCourrier', []).
+angular.module('gestionCourrier', ['$strap.directives']).
     config(function ($routeProvider) {
         $routeProvider.
             when('/', {redirectTo: '/residents'}).
