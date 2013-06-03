2f57c2d099dfa1207bb71d264f26a2d6cc0aa35b
Alban Dericbourg <alban@dericbourg.net>
2013-06-03 19:06:44 +0200

    Bugfix: JsonMappingException: Infinite recursion.

--- app/models/residence/Residence.java
+++ app/models/residence/Residence.java
@@ -2,6 +2,7 @@ package models.residence;
 
 import javax.persistence.*;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
 import org.joda.time.LocalDate;
 
 import play.db.ebean.Model;
@@ -19,6 +20,7 @@ public class Residence extends Model {
     public LocalDate startDate;
     public LocalDate endDate;
     public String mailForwardAddress;
+    @JsonIgnore
     @ManyToOne
     public Resident resident;
 
