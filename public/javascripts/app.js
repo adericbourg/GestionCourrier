5b93ae4509a4ca512a6ccd2cf30bc866f44ab20f
Alban Dericbourg <alban@dericbourg.net>
2013-06-05 18:13:16 +0200

    Navigation in sidebar: automatically handle modules in application and set "active" flag where needed.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,12 +3,19 @@
 var app = angular.module('gestionCourrier', ['ui.bootstrap', '$strap.directives']).
     config(function ($routeProvider) {
         $routeProvider.
-            when('/', {redirectTo: '/residents'}).
-            when('/residents/', {controller: ResidentCtrl, templateUrl: '/assets/partials/resident/residents.html'}).
-            when('/resident/:residentId/', {controller: ViewResidentCtrl, templateUrl: '/assets/partials/resident/viewResident.html'}).
-            when('/resident/:residentId/edit/', {controller: EditResidentCtrl, templateUrl: '/assets/partials/resident/editResident.html'}).
+            when('/', {redirectTo: '/resident/all'}).
+            when('/resident/all', {controller: ResidentCtrl, templateUrl: '/assets/partials/resident/residents.html'}).
+            when('/resident/:residentId', {controller: ViewResidentCtrl, templateUrl: '/assets/partials/resident/viewResident.html'}).
+            when('/resident/:residentId/edit', {controller: EditResidentCtrl, templateUrl: '/assets/partials/resident/editResident.html'}).
             otherwise({redirectTo: '/'})
     });
+
+app.controller('NavigationCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1).split('/')[0] || 'resident';
+        return page === currentRoute ? 'active' : '';
+    };
+}]);
 
 app.value('$strapConfig', {
     datepicker: {
