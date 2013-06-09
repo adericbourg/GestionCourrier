c2424d7fba2777796728d3b14cd3629cabca839b
Alban Dericbourg <alban@dericbourg.net>
2013-06-09 17:53:18 +0200

    LocalDate deserializer: more permissive (allows to manually enter a date).

--- app/core/io/serialization/JodaLocalDateDeserializer.java
+++ app/core/io/serialization/JodaLocalDateDeserializer.java
@@ -14,37 +14,48 @@ import org.joda.time.format.ISODateTimeFormat;
 /**
  * @author adericbourg
  */
-public class JodaLocalDateDeserializer extends JodaDeserializers.LocalDateDeserializer {
+public class JodaLocalDateDeserializer extends
+        JodaDeserializers.LocalDateDeserializer {
 
-    private final static DateTimeFormatter _localDateTimeFormat = ISODateTimeFormat.localDateOptionalTimeParser();
+    private final static DateTimeFormatter _localDateTimeFormat = ISODateTimeFormat
+            .localDateOptionalTimeParser();
+    public static final String FRENCH_DATE_FORMAT = "^("
+            + "("
+            + "(((0[1-9])|(1\\d)|(2[0-8]))\\/((0[1-9])|(1[0-2])))"
+            + "|((31\\/((0[13578])|(1[02])))|((29|30)\\/((0[1,3-9])|(1[0-2]))))"
+            + ")" + "\\/" + "((20[0-9][0-9])|(19[0-9][0-9])))" + "|"
+            + "((29\\/02\\/(19|20)(([02468][048])|([13579][26]))))$";
 
     @Override
-    public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+    public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException {
         {
             // We'll accept either long (timestamp) or array:
             if (jp.isExpectedStartArrayToken()) {
-                jp.nextToken(); // VALUE_NUMBER_INT 
+                jp.nextToken(); // VALUE_NUMBER_INT
                 int year = jp.getIntValue();
                 jp.nextToken(); // VALUE_NUMBER_INT
                 int month = jp.getIntValue();
                 jp.nextToken(); // VALUE_NUMBER_INT
                 int day = jp.getIntValue();
                 if (jp.nextToken() != JsonToken.END_ARRAY) {
-                    throw ctxt.wrongTokenException(jp, JsonToken.END_ARRAY, "after LocalDate ints");
+                    throw ctxt.wrongTokenException(jp, JsonToken.END_ARRAY,
+                            "after LocalDate ints");
                 }
                 return new LocalDate(year, month, day);
             }
             switch (jp.getCurrentToken()) {
-            case VALUE_NUMBER_INT:
-                return new LocalDate(jp.getLongValue());
-            case VALUE_STRING:
-                DateTime local = parseLocal(jp);
-                if (local == null) {
-                    return null;
-                }
-                return local.toLocalDate();
+                case VALUE_NUMBER_INT:
+                    return new LocalDate(jp.getLongValue());
+                case VALUE_STRING:
+                    DateTime local = parseLocal(jp);
+                    if (local == null) {
+                        return null;
+                    }
+                    return local.toLocalDate();
             }
-            throw ctxt.wrongTokenException(jp, JsonToken.START_ARRAY, "expected JSON Array, String or Number");
+            throw ctxt.wrongTokenException(jp, JsonToken.START_ARRAY,
+                    "expected JSON Array, String or Number");
         }
     }
 
@@ -53,7 +64,16 @@ public class JodaLocalDateDeserializer extends JodaDeserializers.LocalDateDeseri
         if (str.length() == 0) { // [JACKSON-360]
             return null;
         }
-        str = str.split("T")[0];
+
+        if (str.matches(FRENCH_DATE_FORMAT)) {
+            // French format.
+            String[] date = str.split("/");
+            str = String.format("%s-%s-%s", date[2], date[1], date[0]);
+        } else {
+            // Default format.
+            str = str.split("T")[0];
+        }
+
         return _localDateTimeFormat.parseDateTime(str);
     }
 }
