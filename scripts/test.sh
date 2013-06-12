0bf66010e3d2d8330ee0be12b309cda4f9b183bd
Alban Dericbourg <alban@dericbourg.net>
2013-06-12 13:45:43 +0200

    Init javascript testing.

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+BASE_DIR=`dirname $0`
+
+echo ""
+echo "Starting Karma Server (http://karma-runner.github.io)"
+echo "-------------------------------------------------------------------"
+
+karma start $BASE_DIR/../test/karma.conf.js $*
---
+++ test/karma.conf.js
@@ -0,0 +1,19 @@
+basePath = '../';
+
+files = [
+    JASMINE,
+    JASMINE_ADAPTER,
+    'public/third-party/angular/angular.js',
+    'public/third-party/angular/angular-*.js',
+    'public/js/*js',
+    'test/unit/**/*.js'
+];
+
+autoWatch = true;
+
+browsers = ['Firefox'];
+
+junitReporter = {
+    outputFile: 'test_out/unit.xml',
+    suite: 'unit'
+};
