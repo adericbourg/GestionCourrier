4fe6f0733e9207e00fd6bb68bb10daa82ecbb207
Alban Dericbourg <alban@dericbourg.net>
2013-06-12 14:14:58 +0200

    Testing.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,13 +3,19 @@ basePath = '../';
 files = [
     JASMINE,
     JASMINE_ADAPTER,
+    'public/third-party/jquery/*.js',
+    'public/third-party/angular/angular.js',
     'public/third-party/angular/angular.js',
     'public/third-party/angular/angular-*.js',
-    'public/js/*.js',
-    'test/unit/*.js',
-    'test/unit/**/*.js',
+    'public/third-party/angular/i18n/angular-*.js',
+    'public/third-party/moment/moment.min.js',
+    'public/third-party/moment/moment-*.js',
+    'public/js/**/*.js',
+    'test/lib/**/*.js',
+    'test/unit/**/*.js'
 ];
 
+colors = true;
 autoWatch = true;
 
 browsers = ['Firefox'];
