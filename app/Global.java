b8a04dfd95c6dcce67afc0aca8d9c8a232618572
Alban Dericbourg <alban@dericbourg.net>
2013-07-12 17:05:09 +0200

    Load fake data on start (if development mode).

---
+++ app/Global.java
@@ -0,0 +1,19 @@
+import play.Application;
+import play.GlobalSettings;
+import play.Play;
+import dev.DevDataFactory;
+
+/**
+ * @author adericbourg
+ */
+public class Global extends GlobalSettings {
+    @Override
+    public void onStart(Application app) {
+        super.onStart(app);
+
+        if (Play.isDev()) {
+            // Create test data.
+            DevDataFactory.createData();
+        }
+    }
+}
